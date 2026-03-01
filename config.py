a5d58b65116c5d20ea7cd11cdd0575b2decbc50f
tgbot69 <kd142047@gmail.com>
2026-03-01 21:51:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "29453152")
+    API_HASH = environ.get("API_HASH", "2302adc174dbc954ae5081eda5131166")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://gd3251791_db_user:zVP87ym5U3T0Ujjd@cluster0.qnt8x1n.mongodb.net/?appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1052054451').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003694223366'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
     
 #Dont Remove My Credit @Silicon_Bot_Update 
